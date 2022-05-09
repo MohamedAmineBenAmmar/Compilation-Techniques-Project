3cb9c81ffabfcb4f032f3b7165bc283f3c55f453
MohamedAmineBenAmmar <ma.benammar5@gmail.com>
2022-05-09 06:29:51 +0200

    start to build the ll1 parsing table

---
+++ syntactic_analyzer/core/ll1_parsing_table/ll1pt.c
@@ -0,0 +1,5 @@
+#include "ll1pt.h"
+
+LL1PT ll1pt_constructor(Grammar grammar, First first, Follow follow){
+    
+}
---
+++ syntactic_analyzer/core/ll1_parsing_table/ll1pt.h
@@ -0,0 +1,9 @@
+#ifndef LL1PT_HEADER_FILE
+#define LL1PT_HEADER_FILE
+
+#include "../../config/types.h"
+
+LL1PT ll1pt_constructor(Grammar grammar, First first, Follow follow);
+void display_ll1pt(LL1PT ll1pt);
+
+#endif
